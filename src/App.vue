5e524a79f3040c2a0563eb9f888e70187926f863
Fio-Fio-Fio <96380120+Fio-Fio-Fio@users.noreply.github.com>
2021-12-19 09:04:25 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -109,36 +109,36 @@ export default {
           "mech": "Forward Without Fear"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "Carolina",
+          "alias": "Dr. Lyra Vickers Jr.",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Necrophile"
+          "mech": "Ozymandias, King of Kings"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
+          "callsign": "Strideshaker",
+          "alias": 'Coralie "Coral" Richelieu',
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "mech": "Earthquaker"
         },
         {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
+          "callsign": "Baron",
+          "alias": "Trine of the 12th Chalice",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Mayfly"
+          "mech": "Worst of All Possible Worlds"
         },
         {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
+          "callsign": "Vixie",
+          "alias": "Vinessa",
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Rio Bravo"
+          "mech": "Clementine"
         },
       ],
       "header": {
@@ -147,10 +147,10 @@ export default {
         "system": "Ardennes-3",
         "gate": "Atlas-Quanokrim",
         "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
-        "headerSubtitle": "Mercenary Company",
-        "subheaderTitle": "Crisis Response",
-        "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
+        "headerTitle": "FOXES",
+        "headerSubtitle": "TOO MANY FOXES",
+        "subheaderTitle": "THE FLUFF",
+        "subheaderSubtitle": "THE FLUFF!!",
       },
       "options":{
         "eventsMarkdownPerMission": true
